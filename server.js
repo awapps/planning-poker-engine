7b399601fcc8716abe43803aa27c88071e03172a
Eberhardt, Andre <santos.eberhardt@gmail.com>
2016-08-31 16:34:05 -0300

    testing heroku deployment

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const server = require('restify').createServer();
 const controller = require('./controller');
 
 controller.setup(server);
-
 server.listen(process.env.PORT || 3000);
 
 module.exports = server;
